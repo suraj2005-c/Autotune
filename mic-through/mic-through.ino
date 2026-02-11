8b316c12f1849b76a0f99bf4eec7421e9c73b7cb
suraj2005-c <suraj.raja-ravi-shankar@insa-lyon.fr>
2026-02-11 11:15:34 +0100

    Add frequency analysis and note detection features

--- mic-through/mic-through.ino
+++ mic-through/mic-through.ino
@@ -4,9 +4,9 @@
 // 1. Matériel
 AudioInputI2S            micInput;       
 AudioOutputI2S           headphones;      
-AudioRecordQueue         queue; // C'est ici qu'on "stocke" temporairement
+AudioRecordQueue         queue; 
 AudioControlSGTL5000     audioShield;
-
+AudioAnalyzeNoteFrequency notefreq;
 // 2. Connexions (Micro -> Casque ET Micro -> Analyseur)
 AudioConnection          patch0(micInput, 0, headphones, 0);
 AudioConnection          patch1(micInput, 0, headphones, 1);
@@ -15,16 +15,19 @@ AudioConnection          patch2(micInput, 0, queue, 0);
 // 3. Buffer de stockage pour le calcul de corrélation
 int16_t storage[512]; 
 int blocksRead = 0;
-
+float noteRef[] = {261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25};
+int scaleSize = 8;
+float freqDetectee, freqCor;
 void setup() {
   Serial.begin(115200);
-  AudioMemory(60); // Assez de mémoire pour ne pas perdre de samples
+  AudioMemory(60); 
 
   audioShield.enable();
   audioShield.inputSelect(AUDIO_INPUT_MIC);
   audioShield.micGain(35);
   audioShield.volume(0.7);
 
+  notefreq.begin(.15);
   queue.begin(); // On commence à remplir la file
   Serial.println("Stockage et calcul activés.");
 }
@@ -63,16 +66,39 @@ void loop() {
           bestLag = lag;
         }
       }
+      
+      if (notefreq.available()){
+        freqDetectee = notefreq.read();
+        freqCor= findClosestNote(freqDetectee);
+      }
+      
 
       // --- ETAPE 3 : AFFICHAGE ---
       if (maxCorr > 1000000) { // On vérifie qu'il y a assez de signal
         float freq = 44100.0 / bestLag;
-        Serial.print("Frequence calculee : ");
-        Serial.print(freq);
+        //Serial.print("Frequence calculee :");
+        //Serial.print(freq);
+        Serial.print("Frequence detect :");
+        Serial.print(freqDetectee);
+        Serial.print("\nFrequence rapportée=");
+        Serial.print(freqCor);
         Serial.println(" Hz");
       }
 
       blocksRead = 0; // On vide le stockage pour recommencer
     }
   }
+}
+
+float findClosestNote(float inputFreq) {
+    float closest = noteRef[0];
+    float minDiff = fabsf(inputFreq - noteRef[0]);
+    for (int i = 1; i < scaleSize; i++) {
+        float diff = fabsf(inputFreq - noteRef[i]);
+        if (diff < minDiff) {
+            minDiff = diff;
+            closest = noteRef[i];
+        }
+    }
+    return closest;
 }
